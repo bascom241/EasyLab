84c5ac56fb6320f2a19c73a00e68db3099b31a4b
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-05-31 15:43:04 +0100

    Deployin Ui

--- ui/src/lib/axios.tsx
+++ ui/src/lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:7000/api",
+    baseURL:"https://easylab.onrender.com/api",
     withCredentials:true
 })
 
