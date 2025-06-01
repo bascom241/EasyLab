bfa85c872ad39fd61b4c9366a579b7c862d1e497
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-06-01 15:06:55 +0100

    Security Configured

--- server/utils/generateTokenAndSetToken.js
+++ server/utils/generateTokenAndSetToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (res, user) => {
     res.cookie("token", token, {
         httpOnly:true,
         secure:process.env.NODE_ENV === "production",
-        sameSite:"strict",
+        // sameSite:"strict",
         maxAge:7*24*60*60*1000
     })
 }
