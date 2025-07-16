c98aa288d8bab40f343616d062487fe704309d3b
Abdulbasit <135514268+bascom241@users.noreply.github.com>
2025-07-16 20:33:59 +0100

    Update middleware.tsx

--- ui/src/middleware.tsx
+++ ui/src/middleware.tsx
@@ -1,1 +1,12 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
+export function middleware(request: NextRequest) {
+  console.log("Welcome Here");
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!_next|static|favicon.ico|api).*)'], // optional
+};
